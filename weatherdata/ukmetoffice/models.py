7aced925ffd5d827e998c8ec0d2a05e30249bfc6
Anusha Lihala <anusha.lihala@koneksys.com>
2023-05-28 22:33:48 +0530

    Update models.py

--- weatherdata/ukmetoffice/models.py
+++ weatherdata/ukmetoffice/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Month(models.Model):
+    name = models.CharField(max_length=3)
+
+class Region(models.Model):
+    name = models.CharField(max_length=50)
+
+class Weather(models.Model):
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    month = models.ForeignKey(Month, on_delete=models.CASCADE)
+    tmax = models.FloatField()
+    tmin = models.FloatField()
+    tmean = models.FloatField()
+    sunshine = models.FloatField()
+    rainfall = models.FloatField()
+    raindays1mm = models.FloatField()
+    airfrost = models.FloatField()
+
+
